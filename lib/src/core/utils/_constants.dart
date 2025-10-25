45aecd16304780d2ed74e9dc8285b25c351bf57c
Seven Du <5564821+medz@users.noreply.github.com>
2025-10-25 22:30:37 +0800

    Add pragma annotations to prefer inlining constants
    The `esc`, `csi`, and `csiSb` constants now have pragma annotations for
    VM, WebAssembly, and Dart2JS to prefer inlining these values during
    compilation.

--- lib/src/core/utils/_constants.dart
+++ lib/src/core/utils/_constants.dart
@@ -1,3 +1,14 @@
+@pragma('vm:prefer-inline')
+@pragma('wasm:prefer-inline')
+@pragma('dart2js:prefer-inline')
 const esc = '\x1B';
+
+@pragma('vm:prefer-inline')
+@pragma('wasm:prefer-inline')
+@pragma('dart2js:prefer-inline')
 const csi = "$esc[";
+
+@pragma('vm:prefer-inline')
+@pragma('wasm:prefer-inline')
+@pragma('dart2js:prefer-inline')
 const csiSb = '\x9B';
