64b1f88b768923e92a0cf954b8a05db253fc571c
Seven Du <5564821+medz@users.noreply.github.com>
2025-10-27 02:53:19 +0800

    Move ShellCompleteDirective to flags module

--- lib/src/tab/command.dart
+++ lib/src/tab/command.dart
@@ -1,14 +1,3 @@
-extension type const ShellCompleteDirective._(int _) {
-  static const none = ShellCompleteDirective._(0);
-  static const error = ShellCompleteDirective._(1 << 0);
-  static const noSpace = ShellCompleteDirective._(1 << 1);
-  static const noFileCompletion = ShellCompleteDirective._(1 << 2);
-  static const filterFileExtension = ShellCompleteDirective._(1 << 3);
-  static const filterDirectory = ShellCompleteDirective._(1 << 4);
-  static const keepOrder = ShellCompleteDirective._(1 << 5);
-  static const maxValue = ShellCompleteDirective._(1 << 6);
-}
-
 typedef CompleteCallback = void Function(String value, String description);
 typedef CompleteHandler<T> =
     void Function(T param, CompleteCallback complete, Map<String, Option>);
