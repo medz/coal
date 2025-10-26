fa643a54816fcdc11ffac8bba794c7249bdbd4b1
Seven Du <5564821+medz@users.noreply.github.com>
2025-10-26 17:45:34 +0800

    Add pragma annotations to scrollLeft and scrollRight functions

--- lib/src/core/utils/scroll.dart
+++ lib/src/core/utils/scroll.dart
@@ -10,6 +10,12 @@ String scrollUp([int count = 1]) => '${csi}S' * count;
 @pragma('dart2js:prefer-inline')
 String scrollDown([int count = 1]) => '${csi}T' * count;
 
+@pragma('vm:prefer-inline')
+@pragma('wasm:prefer-inline')
+@pragma('dart2js:prefer-inline')
 String scrollLeft([int count = 1]) => '${csi}Z' * count;
 
+@pragma('vm:prefer-inline')
+@pragma('wasm:prefer-inline')
+@pragma('dart2js:prefer-inline')
 String scrollRight([int count = 1]) => '${csi}A' * count;
